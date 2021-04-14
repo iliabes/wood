45480bab3d0f51f547f3e483d85a5ead0fbc3045
f0rGetFull <f0rgetful@yandex.ru>
2021-04-14 03:33:29 +0300

    refr

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,12 @@
+
+
 const cellWidth = 50;
 const cellHeight = 50
 let deep = 5;
-let globalX = 100;
+let globalX ;
 let globalY = 5;
-let visibleBlockNumber = 10;
-let leftBlock = 3;
+let visibleBlockNumber = 12;
+let leftBlock = 1;
 let step = 10;
 
 let app = new PIXI.Application({
@@ -22,27 +24,32 @@ app.stage.addChild(container);
 document.body.appendChild(app.view);
 
 
-
 let world = [
-  'g','t','g','g','t','g','g','g','t','t','g','g','g','t','g','t','','t','g','g','g','g','g'
+  'g','t','g','g','t','g','g','g','t','t','g','g','g','t','g','t','','t','g','g',
 ]
-
+globalX = Math.round(world.length/2) * 50;
 let grounds = []
 
 
 let player = new PIXI.Graphics()
-player.x =globalX ;
-player.y =globalY ;
+player.x = 150;
+player.y = 130 ;
 player.lineStyle(10,0x00ff00)
 player.beginFill(0x66CCFF);
 player.drawRect(100,100,10,10)
 player.endFill()
 app.stage.addChild(player)
 
+let ramka = new PIXI.Graphics()
+ramka.lineStyle(1,0x00ff00)
+ramka.drawRect(150,100,500,200)
+app.stage.addChild(ramka)
+
+
 
 //constructor world
 function beginWorl(){
-  for(let i = 0;i<visibleBlockNumber; i++){
+  for(let i = world.length/2 - visibleBlockNumber/2;i<visibleBlockNumber; i++){
     sortElem(world[i],i)
 }
 }
@@ -70,7 +77,7 @@ function createBlock(){
 function createGround(x,y){
   let text = new PIXI.Text('G',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
   let ground  = new PIXI.Graphics()
-  ground.x = x + globalX;
+  ground.x = x + globalX- 1050;
   ground.y = y;
   ground.lineStyle(1,0x00ff00)
   ground.drawRect(0,0,cellWidth,cellHeight)
@@ -85,7 +92,7 @@ function createGround(x,y){
 function createTree(x,y){
   let text = new PIXI.Text('T',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
   let tree = new PIXI.Graphics()
-  tree.x = x + globalX;
+  tree.x = x + globalX - 1050;
   tree.y = y;
   tree.lineStyle(1,0x00ff00)
   tree.drawRect(0,0,cellWidth,cellHeight)
@@ -101,11 +108,11 @@ function createTree(x,y){
 function movePlayer(e){
   switch (e.key) {
     case "a":
-      ShiftRight()
+      ShiftLeft()
       // createLeftBlock()
       break;
     case "d":
-      ShiftLeft()
+      ShiftRight()
       createRightBlock()
       break;
     case "s":
@@ -116,24 +123,39 @@ function movePlayer(e){
 }
 
 function ShiftRight(){
-  grounds.forEach((elem)=>{
-    elem.x += step
-  })
-  globalX += step
+  if(globalX >= 0){
+    grounds.forEach((elem)=>{
+      elem.x -= step
+    })
+    globalX -= step
+  }
 }
 
 function ShiftLeft(){
-  grounds.forEach((elem)=>{
-    elem.x -= step
-  })
-  globalX -= step
+  if(globalX <= world.length * cellWidth){
+    grounds.forEach((elem)=>{
+      elem.x += step
+    })
+    globalX += step
+  }
+
 }
 
 function createRightBlock(){
-  console.log('123123')
+  console.log("globalX:"+globalX)
+  console.log("positionBlodk:"+leftBlock)
   if(globalX/50 >= leftBlock){
-    console.log("globalX:"+globalX)
-    console.log("positionBlodk:"+leftBlock)
+    console.log('a')
+    leftBlock += 1;
+    createBlock()
+  }
+}
+
+function createLeftBlock(){
+  console.log('aaa')
+  if(globalX/50 >= leftBlock){
+    console.log(globalX)
+    console.log(leftBlock)
     console.log('a')
     leftBlock += 1;
     createBlock()
@@ -141,30 +163,7 @@ function createRightBlock(){
   }
 }
 
-// function createLeftBlock(){
-//   console.log('aaa')
-//   if(globalX/50 >= leftBlock){
-//     console.log(globalX)
-//     console.log(leftBlock)
-//     console.log('a')
-//     leftBlock += 1;
-//     createBlock()
-//     deleteFirstBlock()
-//   }
-// }
 
-function contrWorldlater(elem,x,y){
-  switch(elem){
-    case't':
-    createTree(x,y)
-    break
-    case'n':
-    break
-    case'g':
-    createGround(x,y)
-    break
-  }
-}
 
 
 
@@ -176,9 +175,6 @@ function deleteLastBlock(){
   grounds[grounds.length-1].destroy()
   grounds.pop()
 }
-
-
-
 
 container.addChild(player)
 window.addEventListener("keydown", movePlayer)
