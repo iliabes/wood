b4122b8953976b03861c6ef628bbb21893b5d5f4
iliabes <oksana14888@gmail.com>
2021-04-17 03:10:20 +0300

    finished refrac

--- public/script/script.js
+++ public/script/script.js
@@ -1,80 +1,158 @@
-const blockWidth = 150;
-const blockHeight = 50
+const blockWidth = 50,
+      blockHeight = 50
 
 let deep = 5;
-let globalX = 0;
-let globalY = 9;
-let visibleBlockNumber = 12 ;
-let rightCount = 0;
-let step = blockWidth / 5;
-let shift = blockWidth;
-let playerGround = 0;
+    globalX = 0;
+    globalY = 9;
+    visibleBlockNumber = 12 ;
+    rightCount = 0;
+    step = blockWidth / 5;
+    shift = blockWidth;
+    playerGround = false;
+
+
+
 let app = new PIXI.Application({
-  width: 500,
-  height: 200,
-  antialias: true,
-  resolution: 1,
-  backgroundColor:0x01000B
+    width: 500,
+    height: 200,
+    antialias: true,
+    resolution: 1,
+    backgroundColor:0x01000B
 })
 
 let game = new PIXI.Container()
 app.renderer.resize(innerWidth, 500);
-let container = new PIXI.Container();
-app.stage.addChild(container);
 document.body.appendChild(app.view);
-
+app.stage.addChild(game)
 
 let world = [
   '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',
-]
-
-world  = []
-function createWorld(n){
-  for(let i = 0; i < n; i++){
-    world.push(''+i)
-  }
-}createWorld(100)
-shift = world.length/2 * blockWidth;
+];
+// let world = []
 let grounds = []
+
+
+;(function() {
+  // for(let i = 0; i < 100; i++){
+  //   world.push(''+i)
+  // }
+  for(let i = 0;i<visibleBlockNumber; i++){
+    createBlock(world[i],i)}
+  })();
+
+
 
 
 let player = new PIXI.Graphics()
 player.x = 150
-player.y = 130
-player.lineStyle(10,0x00ff00)
+player.y = 330
+player.lineStyle(1,0x00ff00)
 player.beginFill(0x66CCFF);
-player.drawRect(0,0,10,10)
+player.drawRect(0,0,20,80)
 player.endFill()
-app.stage.addChild(player)
+game.addChild(player);
 
-
-// let ramka = new PIXI.Graphics()
-// ramka.lineStyle(1,0x00ff00)
-// ramka.drawRect(50,100,500,200)
-// app.stage.addChild(ramka)
-
-
-
-//constructor world
-function beginWorld(){
-  for(let i = 0;i<visibleBlockNumber; i++){
-    createBlock(world[i],i)
+let keys = {
+  'a':false,
+  'd':false,
+  'space':false,
 }
-}beginWorld()
+
+function movePlayerDown(e){
+  console.log(e.keyCode)
+  switch (e.keyCode) {
+    case 65:
+      keys.a = true
+      createLeftBlock()
+      break;
+    case 68:
+      keys.d = true
+      createRightBlock()
+      break;
+    case 83:
+      moveDown()
+      break;
+    case 32:
+      keys.space = true
+      jump()
+      break;
+  }
+}
+
+function movePlayerUp(e){
+  switch (e.keyCode) {
+    case 65:
+      keys.a = false
+      break;
+    case 68:
+      keys.d = false
+      break;
+    case 83:
+      break;
+    case 32:
+      keys.space = false
+      break;
+  }
+}
+
+
+function moveing(){
+  if(keys.a && !keys.d){
+    console.log('moving')
+    if(globalX < 0 - step){
+      grounds.forEach((elem)=>{
+        elem.x += step
+      })
+      globalX += step
+    }
+  }
+  if(keys.d && !keys.a){
+    if(Math.abs(globalX) < world.length * blockWidth - visibleBlockNumber * blockWidth){
+      grounds.forEach((elem)=>{
+        elem.x -= step
+      })
+      globalX -= step
+    } 
+
+  }
+}
+
+
+
+function moveDown(){
+  if(!playerGround ){
+    player.y += 5
+  }
+}
+
+
+
+function jump(){
+  count = 0
+  playerGround = true
+  let jumpInterval = setInterval(()=>{
+    player.y-=10
+    count++
+    if(count > 10){
+      playerGround = false
+      count = 0
+      clearInterval(jumpInterval)
+    }
+  },100)
+}
 
 function createBlock(elem,x,unshift){
-  console.log('create block:'+elem+ "/"+ x)
-  let text = new PIXI.Text(elem,{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'})
   let block = new PIXI.Graphics()
-  block.x = x * blockWidth + globalX - blockWidth
+  block.x = x * blockWidth + globalX - blockWidth + 150
   block.y = globalY * blockHeight
   block.lineStyle(1,0x00ff00)
   block.drawRect(0,0,blockWidth,blockHeight)
   block.width = blockWidth
+  block.endFill()
+  game.addChild(block)
+  let text = new PIXI.Text(elem,{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'})
   text.x = block.width/2 - text.width/2
   text.y = block.height/2 - text.height/2
-  block.endFill()
-  app.stage.addChild(block)
   block.addChild(text)
   if(unshift){
     grounds.unshift(block)
@@ -83,47 +161,11 @@ function createBlock(elem,x,unshift){
   }
 }
 
-//constructor world
-function movePlayer(e){
-  switch (e.key) {
-    case "a":
-      ShiftLeft()
-      createLeftBlock()
-      break;
-    case "d":
-      ShiftRight()
-      createRightBlock()
-      break;
-    case "s":
-      moveDown()
-      break;
-    case "w":
-      jump()
-      break;
-  }
-}
-
-function ShiftRight(){
-  if(Math.abs(globalX)<= world.length * blockWidth - visibleBlockNumber * blockWidth){
-    grounds.forEach((elem)=>{
-      elem.x -= step
-    })
-    globalX -= step
-  }
-}
-
-function ShiftLeft(){
-  if(globalX < 0 - step){
-    grounds.forEach((elem)=>{
-      elem.x += step
-    })
-    globalX += step
-  }
-}
 
 function createRightBlock(){
-  console.log('rightCount:'+rightCount,'globalX:' + globalX)
+  //первое if видимые блоки то остаеся в рамке
   if(Math.abs(globalX)<= world.length * blockWidth - visibleBlockNumber * blockWidth && globalX< 0 - step){
+    //счетчик сооношения номеров блоков
     if(Math.abs(globalX/blockWidth) >= rightCount + 1 ){
       createBlock(world[visibleBlockNumber + rightCount],visibleBlockNumber + rightCount)
       rightCount += 1
@@ -134,9 +176,10 @@ function createRightBlock(){
 
 function createLeftBlock(){
   console.log('rightCount:'+rightCount,'globalX:' + globalX)
+  //не больше нуля потомучто в начале 
   if(globalX < 0 - step){
+     //счетчик сооношения номеров блоков
     if(Math.abs(globalX/blockWidth) <= rightCount ){
-      console.log('create')
       createBlock(world[rightCount], rightCount,1)
       rightCount -= 1;
       deleteLastBlock()
@@ -158,38 +201,28 @@ function deleteLastBlock(){
 
 function collision(){
   grounds.forEach((object)=>{
-    let playerWidth =  player.width
-    let playerHeight =  player.height
-      // console.log((object.x+object.width/2),(player.x + playerWidth/2))
-       if((player.x + playerWidth/2) >= (object.x - object.width/2) && (player.x - playerWidth/2) <= (object.x+object.width/2) &&(player.y - player.height/2 <=object.y + object.height/2  ) && (player.y + player.height/2 >=object.y - object.height/2  )   ){
-         console.log('collaid')
+       if((player.x + player.width >= object.x) && (player.x <= object.x+object.width) 
+       &&(player.y + player.height >=object.y ) && (player.y  <=object.y + object.height)){
          playerGround = 1;
        }
   })
 }
 
-function moveDown(){
-  if(!playerGround){
-    player.y += 10
-  }
-}
 
-function jump(){
-  for(let i = 0; i < 3;i++ ){
-    player.y -= 10
-  }
-}
-
-setInterval(()=>{
-  moveDown()
-  collision()
-},200)
-container.addChild(player)
-window.addEventListener("keydown", movePlayer)
+let gameInterval = setInterval(()=>{
+      moveDown()
+      collision()
+      moveing()
+    },100)
+window.addEventListener("keyup", movePlayerUp)
+window.addEventListener("keydown", movePlayerDown)
 
 
 
-
+// let ramka = new PIXI.Graphics()
+// ramka.lineStyle(1,0x00ff00)
+// ramka.drawRect(50,100,500,200)
+// app.stage.addChild(ramka)
 
 
 // function createGround(x,y){
@@ -224,10 +257,23 @@ window.addEventListener("keydown", movePlayer)
 
 
 
+// function ShiftRight(){
+//   if(Math.abs(globalX)<= world.length * blockWidth - visibleBlockNumber * blockWidth){
+//     grounds.forEach((elem)=>{
+//       elem.x -= step
+//     })
+//     globalX -= step
+//   }
+// }
 
-
-
-
+// function ShiftLeft(){
+//   if(globalX < 0 - step){
+//     grounds.forEach((elem)=>{
+//       elem.x += step
+//     })
+//     globalX += step
+//   }
+// }
 
 
 
